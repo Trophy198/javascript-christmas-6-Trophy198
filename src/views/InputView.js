15a131e845f1d38feb3506a37174d2d49b511f8b
Trophy198 <spsp8426@naver.com>
2023-11-10 01:32:16 +0900

    feat : 입력값에 대한 유효성 검증 추가

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,17 +1,30 @@
 import { Console } from '@woowacourse/mission-utils';
+import { InputValidator } from '../validators/InputValidator.js';
 
 export default class InputView {
   static async readDate() {
-    const date = await Console.readLineAsync(
-      '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n',
-    );
-    return date;
+    while (true) {
+      try {
+        const input = await Console.readLineAsync(
+          '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n',
+        );
+        return InputValidator.Day.validateDay(input);
+      } catch (error) {
+        Console.print(`${error.message}\n`);
+      }
+    }
   }
 
   static async readMenu() {
-    const menu = await Console.readLineAsync(
-      '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n',
-    );
-    return menu;
+    while (true) {
+      try {
+        const input = await Console.readLineAsync(
+          '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n',
+        );
+        return InputValidator.Menu.validateMenu(input);
+      } catch (error) {
+        Console.print(`${error.message}\n`);
+      }
+    }
   }
 }
