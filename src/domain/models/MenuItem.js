4926b3d3750d6bac24200eded9ca45318c6259ef
Trophy198 <spsp8426@naver.com>
2023-11-10 16:51:38 +0900

    feat : 메뉴 아이템과 주문에 대한 모델 클래스 추가

---
+++ src/domain/models/MenuItem.js
@@ -0,0 +1,7 @@
+export default class MenuItem {
+  constructor(name, price, quantity) {
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+  }
+}
---
+++ src/domain/models/Order.js
@@ -0,0 +1,5 @@
+export default class Order {
+  constructor(menuItems) {
+    this.menuItems = menuItems;
+  }
+}
