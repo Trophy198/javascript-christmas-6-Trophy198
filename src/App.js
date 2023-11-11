21b5fdaab900940d6e49a9155803ef807d3b40e2
Trophy198 <spsp8426@naver.com>
2023-11-12 00:06:24 +0900

    feat : 컨트롤러 시작시 OrderService와 DiscountService 인스턴스 생성 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import EventPlannerController from './controllers/EventPlannerController.js';
+import OrderService from './domain/services/OrderService';
+import DiscountService from './domain/services/DiscountService';
+import EventPlannerController from './controllers/EventPlannerController';
 
 export default class App {
   async run() {
-    const eventPlannerController = new EventPlannerController();
+    const orderService = new OrderService();
+    const discountService = new DiscountService();
+
+    const eventPlannerController = new EventPlannerController(orderService, discountService);
     await eventPlannerController.start();
   }
 }
