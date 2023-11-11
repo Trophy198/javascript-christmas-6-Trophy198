046547bf63df68122941470a15968a6d43688967
Trophy198 <spsp8426@naver.com>
2023-11-12 00:08:39 +0900

    feat : OutputView 주문과 할인 뱃지와 관련된 Output 추가
    - 주문 메뉴 출력
    - 할인 전 총 주문 금액 출력
    - 증정 메뉴 출력
    - 혜택 내역 출력
    - 총혜택 금액 출력
    - 할인 후 예쌍 결제 금액 출력
    - 12월 이벤트 뱃지 출력

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -2,27 +2,56 @@ import { Console } from '@woowacourse/mission-utils';
 
 export default class OutputView {
   static printWelcomeMessage() {
-    Console.print('안녕하세요! 우테코 식장 12월 이벤트 플래너 입니다.');
+    Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
   }
 
-  static printShowEventBenefits(day) {
+  static printOrderDate(day) {
     Console.print(`12월 ${day}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`);
   }
 
-  static printMenu() {
+  static printOrderMenu(menuItems) {
     Console.print('<주문 메뉴>');
+    menuItems.forEach((item) => {
+      Console.print(`${item.name} ${item.quantity}개`);
+    });
   }
-  // 할인 전 총주문 금액
 
-  // 증정 메뉴
+  static printTotalPriceBeforeDiscount(totalPrice) {
+    Console.print('\n<할인 전 총주문 금액>');
+    Console.print(`${totalPrice.toLocaleString()}원`);
+  }
 
-  // 혜택 내역
+  static printGiftItem(giftItem) {
+    Console.print('\n<증정 메뉴>');
+    Console.print(giftItem ? `${giftItem.name} 1개` : '없음');
+  }
 
-  // 총혜택 금액
+  static printDiscountDetails(discounts) {
+    Console.print('\n<혜택 내역>');
+    if (discounts.length > 0) {
+      discounts.forEach((discount) => {
+        Console.print(`${discount.description}: -${discount.amount.toLocaleString()}원`);
+      });
+      return;
+    }
+    Console.print('없음');
+  }
 
-  // 할인 후 예상 결제 금액
+  static printTotalDiscountAmount(totalDiscount) {
+    Console.print('\n<총혜택 금액>');
+    const displayAmount = totalDiscount > 0 ? `-${totalDiscount.toLocaleString()}원` : '0원';
+    Console.print(displayAmount);
+  }
 
-  // 12월 이벤트 배지
+  static printFinalPriceAfterDiscount(finalPrice) {
+    Console.print('\n<할인 후 예상 결제 금액>');
+    Console.print(`${finalPrice.toLocaleString()}원`);
+  }
+
+  static printEventBadge(badge) {
+    Console.print('\n<12월 이벤트 배지>');
+    Console.print(badge || '없음');
+  }
 
   static printErrorMessage(message) {
     Console.print(message);
