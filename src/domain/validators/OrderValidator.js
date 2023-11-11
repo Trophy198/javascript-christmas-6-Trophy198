439f7b5467d5a6a1ca969488a7a168a7e7411076
Trophy198 <spsp8426@naver.com>
2023-11-11 23:53:45 +0900

    refactor : 에러 메시지를 요구 사항에 맞게 수정
    - 에러메시지를 요구사항에 있는 메시지와 맞게 수정하였습니다.

--- src/domain/validators/OrderValidator.js
+++ src/domain/validators/OrderValidator.js
@@ -1,4 +1,4 @@
-import { MENU_ITEMS } from '../../constants/Menu.js';
+import { MENU_ITEMS } from '../../constants/Menu';
 
 export default class OrderValidator {
   static validateOrderItems(orderItems) {
@@ -12,7 +12,7 @@ export default class OrderValidator {
   static validateQuantities(orderItems) {
     orderItems.forEach((item) => {
       if (item.quantity < 1) {
-        throw new Error('[ERROR] 각 메뉴의 수량은 1 이상이어야 합니다.');
+        throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
       }
     });
   }
@@ -22,7 +22,7 @@ export default class OrderValidator {
 
     const uniqueNames = new Set(names);
     if (names.length !== uniqueNames.size) {
-      throw new Error('[ERROR] 중복된 메뉴 이름이 있습니다.');
+      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
     }
   }
 
@@ -31,7 +31,7 @@ export default class OrderValidator {
 
     orderItems.forEach((item) => {
       if (!allMenuItems.some((menuItem) => menuItem.name === item.name)) {
-        throw new Error('[ERROR] 메뉴판에 없는 메뉴가 포함되어 있습니다.');
+        throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
       }
     });
   }
@@ -39,7 +39,7 @@ export default class OrderValidator {
   static validateTotalQuantity(orderItems) {
     const totalQuantity = orderItems.reduce((sum, item) => sum + item.quantity, 0);
     if (totalQuantity > 20) {
-      throw new Error('[ERROR] 주문 가능한 메뉴 수량을 초과했습니다 (최대 20개).');
+      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
     }
   }
 
@@ -47,7 +47,7 @@ export default class OrderValidator {
     const drinkNames = MENU_ITEMS.drinks.map((drink) => drink.name);
 
     if (orderItems.every((item) => drinkNames.includes(item.name))) {
-      throw new Error('[ERROR] 음료만으로는 주문할 수 없습니다.');
+      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
     }
   }
 }
--- src/validators/InputValidator.js
+++ src/validators/InputValidator.js
@@ -4,12 +4,12 @@ export const InputValidator = {
       const dayPattern = /^\d+$/;
 
       if (!dayPattern.test(input)) {
-        throw new Error('[ERROR] 입력값은 숫자만 포함해야 합니다.');
+        throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
       }
 
       const day = parseInt(input, 10);
       if (day < 1 || day > 31) {
-        throw new Error('[ERROR] 날짜는 1 이상 31 이하의 숫자여야 합니다.');
+        throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
       }
 
       return day;
