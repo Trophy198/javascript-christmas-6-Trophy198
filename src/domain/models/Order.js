24b7526501f0ba93af6e2b74a5497d453a9a4d8a
Trophy198 <spsp8426@naver.com>
2023-11-12 00:05:10 +0900

    feat : Order 클래스에서 할인,계산과 관련된 로직 추가
    - 초기 메뉴 아이템에서 가격과 수량을 토대로 할인 전 총 주문 금액을 계산합니다.
    - 할인되는 객체를 생성합니다.
    - 증정 메뉴를 생성합니다.
    - 배지를 생성합니다.
    - 총혜택 금액을 계산 합니다.
    - 총 혜택을 받는 금액을 계산합니다.
    - 할인 후 예상 결제 금액을 계산합니다.

--- src/domain/models/Order.js
+++ src/domain/models/Order.js
@@ -7,7 +7,35 @@ export default class Order {
     this.badge = null;
   }
 
+  addDiscount(description, amount) {
+    this.discounts.push({ description, amount });
+  }
+
+  calculateTotalDiscount() {
+    return this.discounts.reduce((total, discount) => total + discount.amount, 0);
+  }
+
+  addGift(gift) {
+    this.gifts.push(gift);
+  }
+
+  setBadge(badge) {
+    this.badge = badge;
+  }
+
   calculateInitialTotalPrice() {
     return this.menuItems.reduce((total, item) => total + item.price * item.quantity, 0);
+  }
+
+  calculateDiscountExcludingGifts() {
+    return this.discounts
+      .filter((discount) => discount.description !== '증정 이벤트')
+      .reduce((total, discount) => total + discount.amount, 0);
+  }
+
+  calculateFinalPrice() {
+    const totalPriceBeforeDiscount = this.calculateInitialTotalPrice();
+    const discountExcludingGifts = this.calculateDiscountExcludingGifts();
+    return totalPriceBeforeDiscount - discountExcludingGifts;
   }
 }
