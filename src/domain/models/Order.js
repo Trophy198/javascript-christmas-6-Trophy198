b2e847a8f020f9079613b5b1a5fa0bd66094d14b
Trophy198 <spsp8426@naver.com>
2023-11-11 20:49:29 +0900

    feat : 할인 전 총 주문 금액 계산 및 Order 객체 초기화
    - 주문시 필요한 객체들과 초기 주문 금액의 총합 메서드를 구현하였습니다.

--- src/domain/models/Order.js
+++ src/domain/models/Order.js
@@ -1,5 +1,13 @@
 export default class Order {
-  constructor(menuItems) {
+  constructor(menuItems, date) {
     this.menuItems = menuItems;
+    this.date = date;
+    this.discounts = [];
+    this.gifts = [];
+    this.badge = null;
+  }
+
+  calculateInitialTotalPrice() {
+    return this.menuItems.reduce((total, item) => total + item.price * item.quantity, 0);
   }
 }
