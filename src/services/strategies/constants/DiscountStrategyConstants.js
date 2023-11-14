a7a0fb99453d5f6904b712bca2b0a637a7231ce4
Trophy198 <spsp8426@naver.com>
2023-11-14 17:12:18 +0900

    feat : 각 혜택 내역에대한 상수 추가

---
+++ src/services/strategies/constants/DiscountStrategyConstants.js
@@ -0,0 +1,7 @@
+export const DiscountStrategyConstants = {
+  SPECIAL_DISCOUNT: '특별 할인',
+  CHRISTMAS_DISCOUNT: '크리스마스 디데이 할인',
+  GIFT_EVENT: '증정 이벤트',
+  WEEKDAY_DISCOUNT: '평일 할인',
+  WEEKEND_DISCOUNT: '주말 할인',
+};
