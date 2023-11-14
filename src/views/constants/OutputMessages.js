5b9000eaad7bd183a291660f172ba434fe28ed76
Trophy198 <spsp8426@naver.com>
2023-11-14 16:39:19 +0900

    feat : 입출력에 관련된 상수 파일 추가

---
+++ src/views/constants/OutputMessages.js
@@ -0,0 +1,11 @@
+export const OutputMessages = {
+  WELCOME_MESSAGE: '안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.',
+  EVENT_PREVIEW: (day) => `12월 ${day}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`,
+  ORDER_MENU_TITLE: '<주문 메뉴>',
+  TOTAL_AMOUNT_TITLE: '\n<할인 전 총주문 금액>',
+  DISCOUNT_DETAILS_TITLE: '\n<혜택 내역>',
+  TOTAL_DISCOUNT_AMOUNT_TITLE: '\n<총혜택 금액>',
+  GIFT_TITLE: '\n<증정 메뉴>',
+  FINAL_AMOUNT_TITLE: '\n<할인 후 예상 결제 금액>',
+  EVENT_BADGE_TITLE: '\n<12월 이벤트 배지>',
+};
