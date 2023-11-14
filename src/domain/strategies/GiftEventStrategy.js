4f567c089133e650bf210b8f0a984fa977a5ca19
Trophy198 <spsp8426@naver.com>
2023-11-14 17:11:24 +0900

    remove : domain과 controllers 폴더 이동
    - 기존 domain은 model 폴더로 이동하였습니다
    
    - 기존 controllers는 controller폴더로 이동하였습니다

--- src/domain/strategies/GiftEventStrategy.js
+++
@@ -1,9 +0,0 @@
-export default class GiftEventStrategy {
-  apply(order) {
-    const GIFT_CHAMPAGNE = { name: '샴페인', price: 25000 };
-    if (order.calculateInitialTotalPrice() >= 120000) {
-      order.addGift(GIFT_CHAMPAGNE);
-      order.addDiscount('증정 이벤트', GIFT_CHAMPAGNE.price);
-    }
-  }
-}
