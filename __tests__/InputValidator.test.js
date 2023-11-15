42d231ce4bce080e6d6a710e1b776ffed855b7a2
Trophy198 <spsp8426@naver.com>
2023-11-15 14:26:43 +0900

    test : InputValidator에 대한 테스트 케이스 수정
    - 양끝에 공백이 있으면 해당 공백을 제거하고 메뉴를 만들어야합니다.
    
    기능 요구 사항에서 보면 공백이 있는 주문이 있습니다. 이 내용을 토대로 각 메뉴에서 양끝에 공백이 있으면 공백을 제거하고 메뉴를 만드는 처리를 하였습니다.

--- __tests__/InputValidator.test.js
+++ __tests__/InputValidator.test.js
@@ -31,19 +31,8 @@ describe('InputValidator 테스트', () => {
       expect(InputValidator.Menu.validateMenu(input)).toEqual(expectedResult);
     });
 
-    test.each([
-      ['티본스테이크,바비큐립-2'],
-      ['티본스테이크-'],
-      ['-1'],
-      ['티본스테이크-1.5'],
-      ['티본스테이크-abc'],
-    ])('유효하지 않은 메뉴 입력에 대해 오류가 발생한다.', (input) => {
-      expect(() => InputValidator.Menu.validateMenu(input)).toThrow(
-        new Error(ValidatorConstants.INVALID_ORDER_ERROR),
-      );
-    });
-    test('메뉴 입력에 공백이 포함된 경우 오류가 발생한다.', () => {
-      const input = '  티본스테이크-1,  바비큐립-2';
+    test('메뉴 입력에서 양 끝에 공백이 있으면 공백을 제거하고 메뉴를 만든다.', () => {
+      const input = '  티본스테이크-1   ,  바비큐립-2  ';
       const expectedResult = [
         { name: '티본스테이크', quantity: 1 },
         { name: '바비큐립', quantity: 2 },
@@ -53,6 +42,18 @@ describe('InputValidator 테스트', () => {
 
     test('메뉴 이름과 갯수 사이에 공백이 있는 경우 오류가 발생한다.', () => {
       const input = '티본스테이크 - 1';
+      expect(() => InputValidator.Menu.validateMenu(input)).toThrow(
+        new Error(ValidatorConstants.INVALID_ORDER_ERROR),
+      );
+    });
+
+    test.each([
+      ['티본스테이크,바비큐립-2'],
+      ['티본스테이크-'],
+      ['-1'],
+      ['티본스테이크-1.5'],
+      ['티본스테이크-abc'],
+    ])('유효하지 않은 메뉴 입력에 대해 오류가 발생한다.', (input) => {
       expect(() => InputValidator.Menu.validateMenu(input)).toThrow(
         new Error(ValidatorConstants.INVALID_ORDER_ERROR),
       );
